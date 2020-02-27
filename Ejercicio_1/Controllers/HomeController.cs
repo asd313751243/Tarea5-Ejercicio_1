1baaf6ef9bfe9c7ff518cdcf44a1145c843eaa31
asd313751243 <asd313751243@gmail.com>
2020-02-26 21:54:18 -0400

    Estructura basica

---
+++ Ejercicio_1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ejercicio_1.Models;
+
+namespace Ejercicio_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Valores a)
+        {
+            return RedirectToAction("Presentar", a);
+        }
+
+        public IActionResult Presentar(Valores a)
+        {
+            return View(a);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ejercicio_1/Models/Valores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Models
+{
+    public class Valores
+    {
+        [Required]
+        public double valor { get; set; }
+    }
+}
